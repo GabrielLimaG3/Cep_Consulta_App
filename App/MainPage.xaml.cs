dd11ec8fbf128f855d24336018b05fa020f9cbd1
Gabriel Lima <126430100+GabrielLimaG3@users.noreply.github.com>
2024-04-07 16:48:47 -0300

    create program

--- App/MainPage.xaml.cs
+++ App/MainPage.xaml.cs
@@ -1,24 +1,49 @@
-﻿namespace App
+﻿using App.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+
+namespace App
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+         
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private  async void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
+            Label label = new Label();
+            label.Text = string.Empty;
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://viacep.com.br/ws/");
+            
+            var res = await client.GetAsync($"{ety_cpe.Text}/json/");
+           
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
+            if (res.IsSuccessStatusCode)
+            {
+                var lp = await res.Content.ReadAsStringAsync();
+                var resjson = JsonConvert.DeserializeObject<CepModel>(lp);
+                lb_cep.Text = resjson.cep;
+                lb_bairro.Text = resjson.bairro;
+                lb_logradouro.Text = resjson.logradouro;
+                lb_ddd.Text = resjson.ddd;
+                lb_uf.Text = resjson.uf;
+                lb_complemento.Text = resjson.complemento;
+            }
             else
-                CounterBtn.Text = $"Clicked {count} times";
+            {
+               
+                
+                lb_cep.Text = "CEP Informado Errado";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            }
+
+
         }
     }
 
